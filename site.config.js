6f2679d2885d7160fa0651398fb10a255a5ce45d
getsolaris <getsolaris.kr@gmail.com>
2025-05-02 21:37:39 +0900

    ee

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "getsolaris",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/avatar.jpg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "Backend Engineer",
     bio: "",
     email: "getsolaris.kr@gmail.com",
