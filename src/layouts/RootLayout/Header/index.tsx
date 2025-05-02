23bb9058e69d7fb2fa4e75fc6126a48bd16b15b0
getsolaris <getsolaris.kr@gmail.com>
2025-05-03 01:15:48 +0900

    ddd

--- src/layouts/RootLayout/Header/index.tsx
+++ src/layouts/RootLayout/Header/index.tsx
@@ -30,6 +30,7 @@ const StyledWrapper = styled.div`
   top: 0;
   background-color: ${({ theme }) => theme.colors.gray2};
   box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
+  opacity: 0.8;
 
   .container {
     display: flex;
